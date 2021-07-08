b1b8d3390c86a3954e872ca93da430f534009308
petaix <pauletaix@gmail.com>
2021-07-08 10:04:58 +0200

    WIP

--- blank-api/src/controllers/index.ts
+++ blank-api/src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './ping.controller';
 export * from './customer-order.controller';
 export * from './customer.controller';
 export * from './order.controller';
+export * from './customer-order.controller';
--- blank-api/src/datasources/blank.datasource.ts
+++ blank-api/src/datasources/blank.datasource.ts
@@ -1,19 +1,31 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
-export class BlankDataSource extends juggler.DataSource {
-  static dataSourceName = 'blank-db';
+const config = {
+  name: 'blank',
+  connector: 'mysql',
+  url: '',
+  host: '192.168.107.140',
+  port: 3306,
+  user: 'root',
+  password: 'ZNvZO0Fyxq1JcaVqh2tRVpFm3uGgv2qW',
+  database: 'eta_db_test_relation'
+};
 
-  constructor() {
-    super({
-      name: 'blank-db',
-      connector: 'mysql',
-      // url: env.TA_DB_URL,
-      host: "192.168.107.140",
-      port: 3306,
-      user: "root",
-      password: "ZNvZO0Fyxq1JcaVqh2tRVpFm3uGgv2qW",
-      database: "eta_db_test_relation",
-    });
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class BlankDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'blank';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.blank', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
   }
 }
-
